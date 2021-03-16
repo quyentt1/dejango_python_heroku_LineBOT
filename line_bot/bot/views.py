8889d4d48b5447f07dee4eee761d9364e3d250b4
Trọng Quyền Trần <trantrongquyenit@gmail.com>
2021-03-16 14:49:21 +0900

    Update views.py

--- line_bot/bot/views.py
+++ line_bot/bot/views.py
@@ -18,16 +18,19 @@ HEADER = {
 }
 
 def index(request):
+    id1 = "f79f15315d3c47789d85ac422b2c4920"
+  
     return HttpResponse("This is bot api.")
 
-def reply_text(reply_token, text):
+
+def reply_text(reply_token, text, userid):
     reply = random.choice(osomatsu_serif)
     payload = {
           "replyToken":reply_token,
           "messages":[
                 {
                     "type":"text",
-                    "text": reply + reply_token
+                    "text": reply + userid
                 }
             ]
     }
@@ -41,9 +44,9 @@ def callback(request):
     for e in request_json['events']:
         reply_token = e['replyToken']  # 返信先トークンの取得
         message_type = e['message']['type']   # typeの取得
-
+        userid = e['message']['id'] 
         if message_type == 'text':
             text = e['message']['text']    # 受信メッセージの取得
-            reply += reply_text(reply_token, text)   # LINEにセリフを送信する関数
+            reply += reply_text(reply_token, text, userid)   # LINEにセリフを送信する関数
 
     return HttpResponse(reply)  # テスト用
